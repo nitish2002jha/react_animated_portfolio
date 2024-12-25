6b86ffe0057479699f73fb52b5a23b965a3c4f47
nitish2002jha <nkjha8391@gmail.com>
2024-12-26 00:24:39 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,27 @@ import Home from "./components/Home";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
-import Aos from "aos";
-import "aos/dist/aos.css"
+import Contact from "./components/Contact"; 
+import Aos from "aos.css";
+import "aos/dist/aos.css";
 
-const App = () =>{
+const App = () => {
   useEffect(() => {
     Aos.init();
-  
-   
-  }, [])
-  
-  return(
-    <>
-    <Navbar/>
-    <div className="container">
-      <Home />
-      <Experience/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
-    </div>
-    </>
-  )
-}
-export default App
+  }, []);
 
- 
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Home />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Contact /> {/* Uppercase usage */}
+      </div>
+    </>
+  );
+};
+
+export default App;
